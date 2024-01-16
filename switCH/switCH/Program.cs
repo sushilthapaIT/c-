38541c675be28bead2d7451f8bbd7d42cc908550
Sushil <kidsushilink@gmail.com>
2024-01-16 00:32:29 -0800

    switch

---
+++ switCH/switCH/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switCH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    Console.WriteLine("small");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Mid");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("semi-mid");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("BIG");
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
